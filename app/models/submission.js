82c726c564d46bf18f820e8a8f18577b3bd23288
Oscar <lagartoverde97@gmail.com>
2020-01-09 18:51:47 +0100

    removed url attr

--- app/models/submission.js
+++ app/models/submission.js
@@ -2,7 +2,7 @@ import DS from 'ember-data';
 const { Model, attr, belongsTo, hasMany } = DS;
 
 export default Model.extend({
-  href: attr('url'),
+  href: attr(),
   organization: belongsTo('bestuurseenheid'),
   publisher: belongsTo('vendor'),
   submittedResource: belongsTo('submission-document'),
