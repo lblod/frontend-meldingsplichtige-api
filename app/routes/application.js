0e6386d847b1dcbb7b4bd92bdb73618fdc9b3802
Felix Ruiz de Arcaute <ruiz.felixruiz@gmail.com>
2020-01-22 22:52:13 +0100

    added current-session and made work

--- app/routes/application.js
+++ app/routes/application.js
@@ -2,8 +2,19 @@ import Route from '@ember/routing/route';
 import ApplicationRouteMixin from 'ember-simple-auth/mixins/application-route-mixin';
 import ENV from 'frontend-meldingsplichtige-api/config/environment';
 import { warn } from '@ember/debug';
+import { inject as service } from '@ember/service';
 
 export default Route.extend(ApplicationRouteMixin, {
+  currentSession: service(),
+
+  beforeModel() {
+    return this._loadCurrentSession();
+  },
+
+  sessionAuthenticated() {
+    this._super(...arguments);
+    this._loadCurrentSession();
+  },
 
   sessionInvalidated() {
     const logoutUrl = ENV['torii']['providers']['acmidm-oauth2']['logoutUrl'];
@@ -15,4 +26,10 @@ export default Route.extend(ApplicationRouteMixin, {
     }
   },
 
+  _loadCurrentSession() {
+    return this.currentSession.load().catch((e) => {
+      warn(e, { id: 'session-load-failure' });
+      this.session.invalidate();
+    });
+  }
 });
---
+++ app/services/current-session.js
@@ -0,0 +1,60 @@
+import Service from '@ember/service';
+import { inject as service } from '@ember/service';
+import { get, computed } from '@ember/object';
+import { task, waitForProperty } from 'ember-concurrency';
+
+export default Service.extend({
+  session: service('session'),
+  store: service('store'),
+  async load() {
+    if (this.get('session.isAuthenticated')) {
+      const session = this.session;
+      const account = await this.store.find('account', get(session, 'data.authenticated.relationships.account.data.id'));
+      const user = await account.get('gebruiker');
+      const group = await this.store.find('bestuurseenheid', get(session, 'data.authenticated.relationships.group.data.id'));
+      const roles = await get(session, 'data.authenticated.data.attributes.roles');
+      this.set('_account', account);
+      this.set('_user', user);
+      this.set('_roles', roles);
+      this.set('_group', group);
+
+      // The naming is off, but account,user,roles are taken for the
+      // promises in a currently public API.
+      this.setProperties({
+        accountContent: account,
+        userContent: user,
+        rolesContent: roles,
+        groupContent: group
+      });
+
+      this.set('canAccessToezicht', this.canAccess('LoketLB-toezichtGebruiker'));
+      this.set('canAccessBbcdr', this.canAccess('LoketLB-bbcdrGebruiker'));
+      this.set('canAccessMandaat', this.canAccess('LoketLB-mandaatGebruiker'));
+      this.set('canAccessBerichten', this.canAccess('LoketLB-berichtenGebruiker'));
+      this.set('canAccessLeidinggevenden', this.canAccess('LoketLB-leidinggevendenGebruiker'));
+      this.set('canAccessPersoneelsbeheer', this.canAccess('LoketLB-personeelsbeheer'));
+      this.set('canAccessSubsidies', this.canAccess('LoketLB-subsidies'));
+    }
+  },
+  canAccess(role) {
+    return this._roles.includes(role);
+  },
+
+  // constructs a task which resolves in the promise
+  makePropertyPromise: task(function * (property) {
+    yield waitForProperty(this, property);
+    return this.get(property);
+  }),
+  // this is a promise
+  account: computed('_account', function() {
+    return this.makePropertyPromise.perform('_account');
+  }),
+  // this contains a promise
+  user: computed('_user', function() {
+    return this.makePropertyPromise.perform('_user');
+  }),
+  // this contains a promise
+  group: computed('_group', function() {
+    return this.makePropertyPromise.perform('_group');
+  })
+});
