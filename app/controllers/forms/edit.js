570c359b1ac45dd698dcebd02d4581d4457f17e4
Felix Ruiz de Arcaute <ruiz.felixruiz@gmail.com>
2020-01-22 17:09:21 +0100

    stub save

--- app/controllers/forms/edit.js
+++ app/controllers/forms/edit.js
@@ -2,10 +2,13 @@ import Controller from '@ember/controller';
 import { action } from '@ember/object';
 import { tracked } from '@glimmer/tracking';
 import { RDF, FORM } from '../../utils/namespaces';
+import { validateForm }  from '../../utils/import-triples-for-form';
 
 export default class FormsEditController extends Controller {
-  // @action
-  // send(){
-  //   const form = store.any(undefined, RDF("type"), FORM("Form"), formGraph);
-  // }
+  @action
+  send(){
+    const options = { ...this.graphs, sourceNode: this.sourceNode, store: this.formStore};
+    const isValid = validateForm(this.form, options);
+    alert(isValid);
+  }
 }
