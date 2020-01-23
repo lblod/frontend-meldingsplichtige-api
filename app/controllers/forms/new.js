d5179b8fb0e911d9fdc57ef8e7aa852005f60c84
Felix Ruiz de Arcaute <ruiz.felixruiz@gmail.com>
2020-01-23 10:36:49 +0100

    added table for debugging purposes

--- app/controllers/forms/new.js
+++ app/controllers/forms/new.js
@@ -2,6 +2,7 @@ import Controller from '@ember/controller';
 import { action } from '@ember/object';
 import { tracked } from '@glimmer/tracking';
 import { validateForm }  from '../../utils/import-triples-for-form';
+import importTriplesForForm from '../../utils/import-triples-for-form';
 
 export default class FormsNewController extends Controller {
   @tracked
@@ -15,6 +16,21 @@ export default class FormsNewController extends Controller {
 
   @tracked
   form;
+
+  @tracked
+  datasetTriples = [];
+
+  @action
+  registerObserver(){
+    this.formStore.registerObserver(() => {
+      this.setTriplesForTable();
+    });
+  }
+
+  @action
+  setTriplesForTable(){
+    this.datasetTriples = importTriplesForForm(this.form, { ...this.graphs, sourceNode: this.sourceNode, store: this.formStore });
+  }
 
   @action
   send(){
