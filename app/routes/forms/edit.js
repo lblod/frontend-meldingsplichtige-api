21468a76c9a32849a52a3fab7881af361c674da6
Felix Ruiz de Arcaute <ruiz.felixruiz@gmail.com>
2020-01-24 10:44:27 +0100

    add and remove table

--- app/routes/forms/edit.js
+++ app/routes/forms/edit.js
@@ -7,16 +7,28 @@ import { RDF, FORM } from '../../utils/namespaces';
 import documentTypeCodelist from '../../utils/codelist/document-type';
 import fetch from 'fetch';
 
+//TODO: clean
 export default class FormsEditRoute extends Route {
 
   async model(params){
+    const formGraph = new rdflib.NamedNode("http://data.lblod.info/form");
+    const sourceGraph = new rdflib.NamedNode(`http://data.lblod.info/${params.id}`); //TODO: not correct
+    const metaGraph = new rdflib.NamedNode("http://data.lblod.info/metagraph");
+    const graphs = { formGraph, sourceGraph, metaGraph  };
+
     const response = await fetch(`/submission-forms/${params.id}`);
     if(response.status !== 200 ){
-      return { form, formData: dilbeek, disclaimer: 'Geen form gevonden, een voorbeeld wordt getoond!' };
+      //example form
+      return { form,
+               formData: dilbeek,
+               graphs: graphs,
+               sourceNode: new rdflib.NamedNode("http://mu.semte.ch/vocabularies/ext/besluitenlijsten/208ee6e0-28b1-11ea-972c-8915ff690069"),
+               disclaimer: 'Geen form gevonden, een voorbeeld wordt getoond!',
+               id: params.id };
     }
     else{
       const formData = await response.json();
-      return { form, formData: formData.source, id: params.id };
+      return { form, formData: formData.source, id: params.id, graphs, sourceNode: null };
     }
   }
 
@@ -24,20 +36,16 @@ export default class FormsEditRoute extends Route {
     super.setupController(controller, model);
 
     controller.formStore = new forkingStore();
-
-    const formGraph = new rdflib.NamedNode("http://data.lblod.info/form");
-    const sourceGraph = new rdflib.NamedNode(`http://data.lblod.info/${model.id}`);
-    const metaGraph = new rdflib.NamedNode("http://data.lblod.info/metagraph");
-    controller.graphs = { formGraph, sourceGraph, metaGraph  };
+    controller.graphs = model.graphs;
 
     //TODO: how do i get this
-    controller.sourceNode = new rdflib.NamedNode("http://data.lblod.info/vocabularies/ext/besluitenlijsten/208ee6e0-28b1-11ea-972c-8915ff690069");
+    controller.sourceNode = model.sourceNode;
 
-    controller.formStore.parse(model.formData, sourceGraph, "text/turtle");
-    controller.formStore.parse(model.form, formGraph, "text/turtle");
-    controller.formStore.parse(documentTypeCodelist, metaGraph, "text/turtle");
+    controller.formStore.parse(model.formData, model.graphs.sourceGraph, "text/turtle");
+    controller.formStore.parse(model.form, model.graphs.formGraph, "text/turtle");
+    controller.formStore.parse(documentTypeCodelist, model.graphs.metaGraph, "text/turtle");
 
-    controller.form = controller.formStore.any(undefined, RDF("type"), FORM("Form"), formGraph);
+    controller.form = controller.formStore.any(undefined, RDF("type"), FORM("Form"), model.graphs.formGraph);
 
   }
 }
