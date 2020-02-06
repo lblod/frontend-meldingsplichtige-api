cfebbf1433b669458941334f0c7e90dc46e83160
Dries Hugaerts <dries.hugaerts@qmino.com>
2020-02-06 16:29:24 +0100

    create date-time component

---
+++ app/components/form/input-fields/date-time/edit.js
@@ -0,0 +1,46 @@
+import Component from '@glimmer/component';
+import { action } from '@ember/object';
+import { tracked } from '@glimmer/tracking';
+import { triplesForPath, validationResultsForField, updateSimpleFormValue } from '../../../../utils/import-triples-for-form';
+
+export default class FormInputFieldsDateTimeEditComponent extends Component {
+  @tracked
+  value = null;
+
+  @tracked
+  errors = [];
+
+  @tracked
+  storeOptions = {};
+
+  @action
+  loadData(){
+    this.storeOptions = {
+      formGraph: this.args.graphs.formGraph,
+      sourceNode: this.args.sourceNode,
+      sourceGraph: this.args.graphs.sourceGraph,
+      metaGraph: this.args.graphs.metaGraph,
+      store: this.args.formStore,
+      path: this.args.field.rdflibPath
+    };
+
+    this.loadValidations();
+    this.loadProvidedValue();
+  }
+
+  loadValidations(){
+    this.errors = validationResultsForField(this.args.field.uri, this.storeOptions).filter(r => !r.valid);
+  }
+
+  loadProvidedValue(){
+    const matches = triplesForPath(this.storeOptions);
+    if(matches.values.length > 0)
+      this.value = matches.values[0].value;
+  }
+
+  @action
+  updateValue(){
+    this.value = new Date();
+    updateSimpleFormValue(this.value, this.storeOptions);
+  }
+}
--- app/helpers/component-for-display-type-edit.js
+++ app/helpers/component-for-display-type-edit.js
@@ -4,7 +4,7 @@ export default helper(function componentForDisplayTypeEdit(displayTypeUri) {
   const mapping = {
     'http://lblod.data.gift/display-types/defaultInput' : `form/input-fields/input/edit`,
     'http://lblod.data.gift/display-types/bestuursorgaanSelect' : `form/input-fields/toezicht-bestuursorgaan-select/edit`,
-    'http://lblod.data.gift/display-types/dateTime' : `form/input-fields/date/edit`, //TODO: make datetime component
+    'http://lblod.data.gift/display-types/dateTime' : `form/input-fields/date-time/edit`,
     'http://lblod.data.gift/display-types/date' : `form/input-fields/date/edit`,
     'http://lblod.data.gift/display-types/typeDossierSelect': `form/input-fields/type-dossier-select/edit`,
     'http://lblod.data.gift/display-types/textArea': `form/input-fields/text-area/edit`
