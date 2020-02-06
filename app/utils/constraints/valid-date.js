7fe52e831cab6d0361ff1b77ce506544da537d0d
Dries Hugaerts <dries.hugaerts@qmino.com>
2020-02-06 10:24:28 +0100

    small fixes

--- app/utils/constraints/valid-date.js
+++ app/utils/constraints/valid-date.js
@@ -5,25 +5,23 @@
  *
  */
 export default function constraintValidDate(value) {
-  console.log(value);
-  var dateString = value.value.split("T")[0];
+  let dateString = value.value.split("T")[0];
 
-  console.log(dateString);
   // First check for the pattern
   if (!/^\d{4}-\d{1,2}-\d{1,2}$/.test(dateString))
     return false;
 
   // Parse the date parts to integers
-  var parts = dateString.split("-");
-  var day = parseInt(parts[2], 10);
-  var month = parseInt(parts[1], 10);
-  var year = parseInt(parts[0], 10);
+  let parts = dateString.split("-");
+  let day = parseInt(parts[2], 10);
+  let month = parseInt(parts[1], 10);
+  let year = parseInt(parts[0], 10);
 
   // Check the ranges of month and year
   if (year < 1000 || year > 3000 || month == 0 || month > 12)
     return false;
 
-  var monthLength = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+  let monthLength = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
 
   // Adjust for leap years
   if (year % 400 == 0 || (year % 100 != 0 && year % 4 == 0))
