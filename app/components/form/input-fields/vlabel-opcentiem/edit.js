6725a60ec43faa85e42651cc327f882300e406c5
Dries Hugaerts <dries.hugaerts@qmino.com>
2020-02-12 14:48:11 +0100

    improved the boilerplate.

--- app/components/form/input-fields/vlabel-opcentiem/edit.js
+++ app/components/form/input-fields/vlabel-opcentiem/edit.js
@@ -7,12 +7,38 @@ import { triplesForPath, validationResultsForField, updateSimpleFormValue } from
 export default class FormInputFieldsVlabelOpcentiemEditComponent extends Component {
 
   @tracked
+  differentiatie;
+
+  @tracked
   taxRates = [];
 
   @empty('taxRates')
   taxRatesEmpty;
 
+
+  @not('differentiatie')
+  showTable;
+
   @reads('taxRatesEmpty')
   showDifferentiatie;
 
+
+  @action
+  toggleDiff(event) {
+    event.preventDefault();
+    console.log("toggled diff");
+    this.differentiatie = !this.differentiatie;
+  }
+
+  @action
+  create(){
+    console.log("create clicked");
+    this.taxRates.pushObject("");
+  }
+
+  @action
+  delete(value) {
+    console.log("delete clicked with value:");
+    console.log(value);
+  }
 }
