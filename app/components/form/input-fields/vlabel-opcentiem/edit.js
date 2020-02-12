bcc918cde112e7f281c3a9d7370d6946eddedcaf
Dries Hugaerts <dries.hugaerts@qmino.com>
2020-02-12 10:16:12 +0100

    start boilerplate component

--- app/components/form/input-fields/vlabel-opcentiem/edit.js
+++ app/components/form/input-fields/vlabel-opcentiem/edit.js
@@ -1,7 +1,18 @@
 import Component from '@glimmer/component';
 import { action } from '@ember/object';
 import { tracked } from '@glimmer/tracking';
+import { oneWay, reads, empty, not} from '@ember/object/computed';
 import { triplesForPath, validationResultsForField, updateSimpleFormValue } from '../../../../utils/import-triples-for-form';
 
 export default class FormInputFieldsVlabelOpcentiemEditComponent extends Component {
+
+  @tracked
+  taxRates = [];
+
+  @empty('taxRates')
+  taxRatesEmpty;
+
+  @reads('taxRatesEmpty')
+  showDifferentiatie;
+
 }
