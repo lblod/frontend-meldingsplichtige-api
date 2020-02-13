07447bb8c1251e161d194bc9c70a3745b44ce01a
Dries Hugaerts <dries.hugaerts@qmino.com>
2020-02-13 13:17:40 +0100

    added show render for simple input field + updated/improved the html.

---
+++ app/components/form/input-fields/input/show.js
@@ -0,0 +1,32 @@
+import Component from '@glimmer/component';
+import { action } from '@ember/object';
+import { tracked } from '@glimmer/tracking';
+import { triplesForPath, validationResultsForField, updateSimpleFormValue } from '../../../../utils/import-triples-for-form';
+
+export default class FormInputFieldsInputShowComponent extends Component {
+  @tracked
+  value = null;
+
+  @tracked
+  storeOptions = {};
+
+  @action
+  loadData(){
+    this.storeOptions = {
+      formGraph: this.args.graphs.formGraph,
+      sourceNode: this.args.sourceNode,
+      sourceGraph: this.args.graphs.sourceGraph,
+      metaGraph: this.args.graphs.metaGraph,
+      store: this.args.formStore,
+      path: this.args.field.rdflibPath
+    };
+    this.loadProvidedValue();
+  }
+
+  loadProvidedValue(){
+    const matches = triplesForPath(this.storeOptions);
+    if(matches.values.length > 0)
+      this.value = matches.values[0].value;
+  }
+
+}
