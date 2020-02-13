e7820614d4699a3be154230a2f7d02c2103081f5
Dries Hugaerts <dries.hugaerts@qmino.com>
2020-02-13 17:07:51 +0100

    added text-area to showable fields + updated html.

--- app/helpers/component-for-display-type-show.js
+++ app/helpers/component-for-display-type-show.js
@@ -3,9 +3,10 @@ import { helper } from '@ember/component/helper';
 export default helper(function componentForDisplayTypeShow(displayTypeUri) {
   const mapping = {
     'http://lblod.data.gift/display-types/defaultInput' : `form/input-fields/input/show`,
+    'http://lblod.data.gift/display-types/textArea' : `form/input-fields/text-area/show`,
     'http://lblod.data.gift/display-types/bestuursorgaanSelect' : `form/input-fields/toezicht-bestuursorgaan-select/show`,
     'http://lblod.data.gift/display-types/typeDossierSelect': `form/input-fields/type-dossier-select/show`,
-    'http://lblod.data.gift/display-types/date' : `form/input-fields/input/show`,
+    'http://lblod.data.gift/display-types/date' : `form/input-fields/input/show`, // TODO proper date component
   };
 
   //TODO: files and links component
