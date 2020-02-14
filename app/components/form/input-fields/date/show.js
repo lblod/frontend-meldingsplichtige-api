d9480c00d8012661d1f9c72582f039b3fd4706d7
Dries Hugaerts <dries.hugaerts@qmino.com>
2020-02-14 11:43:54 +0100

    improved show.js for date component.

--- app/components/form/input-fields/date/show.js
+++ app/components/form/input-fields/date/show.js
@@ -1,32 +1,5 @@
-import Component from '@glimmer/component';
-import { action } from '@ember/object';
-import { tracked } from '@glimmer/tracking';
-import { triplesForPath} from '../../../../utils/import-triples-for-form';
+import FormInputFieldsDateEditComponent from "./edit";
 
-export default class FormInputFieldsDateShowComponent extends Component {
-  @tracked
-  value = null;
+export default class FormInputFieldsDateShowComponent extends FormInputFieldsDateEditComponent {
 
-  @tracked
-  storeOptions = {};
-
-  @action
-  loadData(){
-    this.storeOptions = {
-      formGraph: this.args.graphs.formGraph,
-      sourceNode: this.args.sourceNode,
-      sourceGraph: this.args.graphs.sourceGraph,
-      metaGraph: this.args.graphs.metaGraph,
-      store: this.args.formStore,
-      path: this.args.field.rdflibPath
-    };
-
-    this.loadProvidedValue();
-  }
-
-  loadProvidedValue(){
-    const matches = triplesForPath(this.storeOptions);
-    if(matches.values.length > 0)
-      this.value = matches.values[0].value;
-  }
 }
