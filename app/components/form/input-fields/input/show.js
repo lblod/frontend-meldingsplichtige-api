6c94771f9e26c41d729a0bd2c7ffa09e0813956c
Dries Hugaerts <dries.hugaerts@qmino.com>
2020-02-14 11:45:31 +0100

    improved show.js for input and text-area component.

--- app/components/form/input-fields/input/show.js
+++ app/components/form/input-fields/input/show.js
@@ -1,32 +1,5 @@
-import Component from '@glimmer/component';
-import {action} from '@ember/object';
-import {tracked} from '@glimmer/tracking';
-import {triplesForPath} from '../../../../utils/import-triples-for-form';
 
-export default class FormInputFieldsInputShowComponent extends Component {
-  @tracked
-  value = null;
+import FormInputFieldsInputEditComponent from "./edit";
 
-  @tracked
-  storeOptions = {};
-
-  @action
-  loadData() {
-    this.storeOptions = {
-      formGraph: this.args.graphs.formGraph,
-      sourceNode: this.args.sourceNode,
-      sourceGraph: this.args.graphs.sourceGraph,
-      metaGraph: this.args.graphs.metaGraph,
-      store: this.args.formStore,
-      path: this.args.field.rdflibPath
-    };
-    this.loadProvidedValue();
-  }
-
-  loadProvidedValue() {
-    const matches = triplesForPath(this.storeOptions);
-    if (matches.values.length > 0)
-      this.value = matches.values[0].value;
-  }
-
+export default class FormInputFieldsInputShowComponent extends FormInputFieldsInputEditComponent {
 }
