5fccf0f71c45ecabf6e757da1bc5541400834ae9
Felix Ruiz de Arcaute <ruiz.felixruiz@gmail.com>
2020-02-18 17:51:15 +0100

    [WIP] - added create new flow

--- app/controllers/forms/new.js
+++ app/controllers/forms/new.js
@@ -4,8 +4,18 @@ import { tracked } from '@glimmer/tracking';
 import { validateForm }  from '../../utils/import-triples-for-form';
 import importTriplesForForm from '../../utils/import-triples-for-form';
 import { delGraphFor, addGraphFor } from '../../utils/forking-store';
+import { inject as service } from '@ember/service';
+import fetch from 'fetch';
+
+const CONCEPT_STATUS = 'http://lblod.data.gift/concepts/79a52da4-f491-4e2f-9374-89a13cde8ecd';
 
 export default class FormsNewController extends Controller {
+  @service
+  currentSession;
+
+  @service
+  router;
+
   @tracked
   formStore;
 
@@ -34,6 +44,34 @@ export default class FormsNewController extends Controller {
     });
   }
 
+  async createSubmission(){
+    const organization = await this.currentSession.group;
+    const status = (await this.store.query('submission-document-status', {'filter[:uri:]': CONCEPT_STATUS})).firstObject;
+    const submission = await this.store.createRecord('submission', {organization, status});
+    await submission.save();
+    return submission;
+  }
+
+  async createSubmissionForm(submission){
+    await fetch(`/submission-forms`, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/vnd.api+json'},
+      body: JSON.stringify(
+        {
+          submission: submission.uri,
+          ...this.formStore.serializeDataWithAddAndDelGraph(this.graphs.sourceGraph)
+        }
+      )
+    });
+  }
+
+  async submitSubmissionForm(submissionDocument){
+    await fetch(`/submission-forms/${submissionDocument.id}/submit`, {
+       method: 'POST',
+      headers: { 'Content-Type': 'application/vnd.api+json'}
+    });
+  }
+
   @action
   setTriplesForTables(){
     this.datasetTriples = importTriplesForForm(this.form, { ...this.graphs, sourceNode: this.sourceNode, store: this.formStore });
@@ -42,9 +80,25 @@ export default class FormsNewController extends Controller {
   }
 
   @action
-  send(){
+  async save(){
+    const submission = await this.createSubmission();
+    await this.createSubmissionForm(submission);
+    this.router.transitionTo('forms.edit', submission.get('id'));
+  }
+
+  @action
+  async submit(){
     const options = { ...this.graphs, sourceNode: this.sourceNode, store: this.formStore};
     const isValid = validateForm(this.form, options);
-    alert(isValid);
+    if(!isValid){
+      alert('Gelieve het formulier correct in te vullen');
+    }
+    else{
+      const submission = await this.createSubmission();
+      await this.createSubmissionForm(submission);
+      const submissionDocument = await submission.submittedResource;
+      await this.submitSubmissionForm(submissionDocument);
+      this.router.transitionTo('forms.edit', submission.get('id'));
+    }
   }
 }
