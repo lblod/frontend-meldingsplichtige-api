6bd6c94a1cb70af50d4cc3506efe94cbfe9ff142
Felix Ruiz de Arcaute <ruiz.felixruiz@gmail.com>
2020-02-19 08:58:20 +0100

    remove commented out code

--- app/routes/forms/edit-example-form.js
+++ app/routes/forms/edit-example-form.js
@@ -10,15 +10,10 @@ import fetch from 'fetch';
 //TODO: clean up
 export default class FormsEditExampleFormRoute extends Route {
 
-  async model(params){
+  async model(){
     const formGraph = new rdflib.NamedNode("http://data.lblod.info/form");
     const metaGraph = new rdflib.NamedNode("http://data.lblod.info/metagraph");
 
-    // const submission = await this.store.find('submission', params.id);
-    // const submissionDocument = await submission.submittedResource;
-
-
-    //default form
     let sourceGraph = new rdflib.NamedNode(`http://data.lblod.info/dilbeek`);
     let graphs = { formGraph, sourceGraph, metaGraph };
     let model = { form,
@@ -28,28 +23,6 @@ export default class FormsEditExampleFormRoute extends Route {
                   disclaimer: 'Sample form'
                   };
     return model;
-    // if(!submissionDocument){
-    //   return model;
-    // }
-
-  //   else{
-  //     sourceGraph = new rdflib.NamedNode(`http://data.lblod.info/submission-document/data/${submissionDocument.id}`);
-  //     graphs = { formGraph, sourceGraph, metaGraph };
-  //     const response = await fetch(`/submission-forms/${submissionDocument.id}`);
-  //     if(response.status !== 200){
-  //       return model;
-  //     }
-  //     const formData = await response.json();
-  //     return { form,
-  //              additions: formData.additions,
-  //              removals: formData.removals,
-  //              formData: formData.source,
-  //              graphs,
-  //              sourceNode: new rdflib.NamedNode(submissionDocument.uri),
-  //              submissionDocument
-  //            };
-  //   }
-  // }
   }
   setupController(controller, model){
     super.setupController(controller, model);
@@ -58,18 +31,6 @@ export default class FormsEditExampleFormRoute extends Route {
     controller.graphs = model.graphs;
 
     controller.sourceNode = model.sourceNode;
-
-    // if(model.removals || model.additions){
-    //   controller.formStore.loadDataWithAddAndDelGraph(model.formData,
-    //                                                   model.graphs.sourceGraph,
-    //                                                   model.additions,
-    //                                                   model.removals,
-    //                                                   "text/turtle");
-    //   controller.formStore.parse(model.formData, model.graphs.sourceGraph, "text/turtle");
-    // }
-    // else {
-    //   controller.formStore.parse(model.formData, model.graphs.sourceGraph, "text/turtle");
-    // }
     controller.formStore.parse(model.formData, model.graphs.sourceGraph, "text/turtle");
     controller.formStore.parse(model.form, model.graphs.formGraph, "text/turtle");
     controller.formStore.parse(codeLists, model.graphs.metaGraph, "text/turtle");
