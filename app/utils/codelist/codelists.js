add88ae5128f7ce550a2a65574e387354b73c7f6
Sergey Andreev <sergey.andreev@redpencil.io>
2020-02-20 18:44:40 +0100

    updated codelists

--- app/utils/codelist/codelists.js
+++ app/utils/codelist/codelists.js
@@ -1,12 +1,12 @@
 //codelists import
-import documentTypeCodelist from './document-type';
+//import documentTypeCodelist from './document-type';
 import authenticityTypeCodelist from './authenticity-type';
 import minimalBelgianChartOfAccountsCodelist from './minimal-belgian-chart-of-accounts';
 import taxTypeCodelist from './tax-type';
 import besluittypesAndTypeDossiersVoorToezicht from './besluittypes-and-type-dossiers-voor-toezicht';
 
 export default
-  documentTypeCodelist+
+  //documentTypeCodelist+
   authenticityTypeCodelist+
   minimalBelgianChartOfAccountsCodelist+
   taxTypeCodelist+
