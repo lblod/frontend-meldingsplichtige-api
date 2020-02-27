fb409c77f155ce6774710d2cc453454dbc1223f0
Felix Ruiz de Arcaute <ruiz.felixruiz@gmail.com>
2020-02-27 16:16:40 +0100

    fetch forms from remote

--- app/routes/forms.js
+++ app/routes/forms.js
@@ -1,18 +1,28 @@
 import Route from '@ember/routing/route';
 import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
-import formTtl from '../utils/form';
-import codeLists from '../utils/codelist/codelists';
 import { RDF, FORM } from '../utils/namespaces';
 import forkingStore from '../utils/forking-store';
 import rdflib from 'ember-rdflib';
+import fetch from 'fetch';
 
 export default Route.extend(AuthenticatedRouteMixin, {
+  async getCodelistsTtl(){
+    const response = await fetch(`/semantic-forms-data/codelists.ttl`);
+    return response.text();
+  },
 
-  model(){
+  async getFormTtl(){
+    const response = await fetch(`/semantic-forms-data/form.ttl`);
+    return response.text();
+  },
+
+  async model(){
+    const codeListsTtl = await this.getCodelistsTtl();
+    const formTtl = await this.getFormTtl();
     const formGraph = new rdflib.NamedNode("http://data.lblod.info/form");
     const metaGraph = new rdflib.NamedNode("http://data.lblod.info/metagraph");
     const formStore = new forkingStore();
-    formStore.parse(codeLists, metaGraph, "text/turtle");
+    formStore.parse(codeListsTtl, metaGraph, "text/turtle");
     formStore.parse(formTtl, formGraph, "text/turtle");
     const form = formStore.any(undefined, RDF("type"), FORM("Form"), formGraph);
     return { formGraph, metaGraph, form, formStore };
