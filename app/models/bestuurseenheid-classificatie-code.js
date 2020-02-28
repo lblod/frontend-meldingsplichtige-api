ea8ac4f00f9ddc3cd12ee0828bdaa3e93c4d58f7
Felix Ruiz de Arcaute <ruiz.felixruiz@gmail.com>
2020-02-28 09:45:49 +0100

    remove non relevant type dossiers

--- app/models/bestuurseenheid-classificatie-code.js
+++ app/models/bestuurseenheid-classificatie-code.js
@@ -2,6 +2,7 @@ import DS from 'ember-data';
 const { Model, attr } = DS;
 
 export default Model.extend({
+  uri: attr('string'),
   label: attr('string'),
   scopeNote: attr('string')
 });
--- app/routes/forms.js
+++ app/routes/forms.js
@@ -4,8 +4,11 @@ import { RDF, FORM } from '../utils/namespaces';
 import forkingStore from '../utils/forking-store';
 import rdflib from 'ember-rdflib';
 import fetch from 'fetch';
+import { inject as service } from '@ember/service';
 
 export default Route.extend(AuthenticatedRouteMixin, {
+  currentSession: service(),
+
   async getCodelistsTtl(){
     const response = await fetch(`/semantic-forms-data/codelists.ttl`);
     return response.text();
@@ -16,6 +19,29 @@ export default Route.extend(AuthenticatedRouteMixin, {
     return response.text();
   },
 
+  /*
+   * For displaying purposes to the end user,
+   * we remove type dossiers which are not relevant to bestuurseenheid.
+   * This is not equal to validation!
+   */
+  async cleanupFormStoreFromNonRelevenatTypeDossiers(formStore, metaGraph){
+    const bestuurseenheid = await this.currentSession.group;
+    const classificatie = await bestuurseenheid.classificatie;
+
+    const classificatieUri = new rdflib.NamedNode(classificatie.uri);
+    const decidableBy = new rdflib.NamedNode('http://lblod.data.gift/vocabularies/besluit/decidableBy');
+    const inScheme = new rdflib.NamedNode('http://www.w3.org/2004/02/skos/core#inScheme');
+    const typeDossierScheme = new rdflib.NamedNode('http://lblod.data.gift/concept-schemes/71e6455e-1204-46a6-abf4-87319f58eaa5');
+
+    const typeDossiersForEenheid = formStore
+          .match(undefined, decidableBy, classificatieUri, metaGraph)
+          .filter(t => formStore.any(t.subject, inScheme, typeDossierScheme, metaGraph)); //In case usage of decidableBy changes;
+    const typeDossiers = formStore.match(undefined, inScheme, typeDossierScheme, metaGraph);
+
+    const dossiersToRemove = typeDossiers.filter(t => !typeDossiersForEenheid.any(td => td.subject.equals(t.subject)));
+    formStore.removeStatements( dossiersToRemove );
+  },
+
   async model(){
     const codeListsTtl = await this.getCodelistsTtl();
     const formTtl = await this.getFormTtl();
@@ -24,6 +50,8 @@ export default Route.extend(AuthenticatedRouteMixin, {
     const formStore = new forkingStore();
     formStore.parse(codeListsTtl, metaGraph, "text/turtle");
     formStore.parse(formTtl, formGraph, "text/turtle");
+
+    await this.cleanupFormStoreFromNonRelevenatTypeDossiers(formStore, metaGraph);
     const form = formStore.any(undefined, RDF("type"), FORM("Form"), formGraph);
     return { formGraph, metaGraph, form, formStore };
   }
