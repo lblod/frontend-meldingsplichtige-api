d42356953335a385c8a87fbf56e619a63c88440a
Felix Ruiz de Arcaute <ruiz.felixruiz@gmail.com>
2020-03-02 18:46:20 +0100

    bugfix

--- app/utils/constraints/valid-date-time.js
+++ app/utils/constraints/valid-date-time.js
@@ -4,7 +4,7 @@ import validDate from './valid-date';
  * Expected datetime format ex: 01-01-2020T01:01:01Z
  */
 export default function constraintValidDateTime(value) {
-  if(!value.datatype.value == "http://www.w3.org/2001/XMLSchema#dateTime"){
+  if(value.datatype.value !== "http://www.w3.org/2001/XMLSchema#dateTime"){
     return false;
  }
   return validDateTimeString(value.value);
