19a59b112130da1d438873970fc71a60af348fd0
Dries Hugaerts <dries.hugaerts@qmino.com>
2020-03-05 17:35:21 +0100

    removed `form/input-fields/toezicht-bestuursorgaan-select/edit` from helpers.

--- app/helpers/component-for-display-type-show.js
+++ app/helpers/component-for-display-type-show.js
@@ -4,7 +4,6 @@ export default helper(function componentForDisplayTypeShow(displayTypeUri) {
   const mapping = {
     'http://lblod.data.gift/display-types/defaultInput' : `form/input-fields/input/show`,
     'http://lblod.data.gift/display-types/textArea' : `form/input-fields/text-area/show`,
-    'http://lblod.data.gift/display-types/bestuursorgaanSelect' : `form/input-fields/toezicht-bestuursorgaan-select/show`,
     'http://lblod.data.gift/display-types/typeDossierSelect': `form/input-fields/type-dossier-select/show`,
     'http://lblod.data.gift/display-types/date' : `form/input-fields/date/show`,
     'http://lblod.data.gift/display-types/dateTime' : `form/input-fields/date-time/show`,
