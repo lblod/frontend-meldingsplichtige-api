af3b2581bf1696b59d87f7c808e34551ff445688
Felix Ruiz de Arcaute <ruiz.felixruiz@gmail.com>
2020-03-05 15:25:27 +0100

    cleanup

--- app/components/form/input-fields/remote-urls/edit.js
+++ app/components/form/input-fields/remote-urls/edit.js
@@ -2,7 +2,6 @@ import Component from '@glimmer/component';
 import {action} from '@ember/object';
 import {tracked} from '@glimmer/tracking';
 import {inject as service} from '@ember/service';
-import {set} from '@ember/object';
 import {
   triplesForPath,
   validationResultsForField,
