b47b3e15a436793fb638147456d013a90cd42ae2
Dries Hugaerts <dries.hugaerts@qmino.com>
2020-03-24 11:04:54 +0100

    updating "remote-url" component to NOT trigger the document-download-service.

--- app/components/form/input-fields/remote-urls/edit.js
+++ app/components/form/input-fields/remote-urls/edit.js
@@ -5,15 +5,15 @@ import {inject as service} from '@ember/service';
 import {
   triplesForPath,
   validationResultsForField,
-  validationResultsForFieldPart,
-  addSimpleFormValue,
-  removeSimpleFormValue
+  validationResultsForFieldPart
 } from '../../../../utils/import-triples-for-form';
 
-import { RDF } from '../../../../utils/namespaces';
+import {DCT, NIE, RDF} from '../../../../utils/namespaces';
 import rdflib from 'ember-rdflib';
+import uuidv4 from "uuid/v4";
 
 const CREATOR_URI = "http://lblod.data.gift/fronted-end-componets/remote-url-creator";
+const REMOTE_URI_TEMPLATE = 'http://data.lblod.info/remote-url/';
 
 export default class FormInputFieldsRemoteUrlsEditComponent extends Component {
 
@@ -54,9 +54,10 @@ export default class FormInputFieldsRemoteUrlsEditComponent extends Component {
 
     for (let uri of matches.values) {
       try {
-        if(!this.isRemoteDataObject(uri)) continue;
-        const remotes = await this.store.query('remote-url', {'filter[:uri:]': uri.value});
-        const remoteUrl = remotes.get('firstObject');
+        if (!this.isRemoteDataObject(uri)) continue;
+        let remotes = await this.store.query('remote-url', {'filter[:uri:]': uri.value});
+        // TODO somehow this objects gets a rdflib syntax??
+        let remoteUrl = remotes.get('firstObject');
         if (remoteUrl) {
           this.remoteUrls.pushObject({
             remoteUrl,
@@ -64,40 +65,65 @@ export default class FormInputFieldsRemoteUrlsEditComponent extends Component {
             uri
           });
         } else {
-          this.remoteErrors.pushObject({resultMessage : "Er ging iets fout bij het ophalen van de addressen."});
+          // TODO try to retrieve it from local + simplify
+          remotes = await this.store.peekAll('remote-url').filter(remote => remote.uri === uri.value);
+          remoteUrl = remotes.get('firstObject');
+          if(remoteUrl){
+            this.remoteUrls.pushObject({
+              remoteUrl,
+              errors: this.validationResultsForAddress(remoteUrl.address),
+              uri
+            });
+          } else {
+            this.remoteErrors.pushObject({resultMessage: "Er ging iets fout bij het ophalen van de addressen."});
+          }
         }
       } catch (error) {
-        this.remoteErrors.pushObject({resultMessage : "Er ging iets fout bij het ophalen van de addressen."});
+        this.remoteErrors.pushObject({resultMessage: "Er ging iets fout bij het ophalen van de addressen."});
       }
 
     }
   }
 
-  isRemoteDataObject(subject){
+  isRemoteDataObject(subject) {
     return this.storeOptions.store.match(subject,
-                                         RDF('type'),
-                                         new rdflib.NamedNode('http://www.semanticdesktop.org/ontologies/2007/03/22/nfo#RemoteDataObject'),
-                                         this.storeOptions.sourceGraph).length > 0;
+      RDF('type'),
+      new rdflib.NamedNode('http://www.semanticdesktop.org/ontologies/2007/03/22/nfo#RemoteDataObject'),
+      this.storeOptions.sourceGraph).length > 0;
   }
 
-  insertRemoteDataObject(remoteObjUri){
-    const typeT = { subject: new rdflib.NamedNode(remoteObjUri),
-                    predicate: RDF('type'),
-                    object: new rdflib.NamedNode('http://www.semanticdesktop.org/ontologies/2007/03/22/nfo#RemoteDataObject'),
-                    graph: this.storeOptions.sourceGraph
-                  };
-    this.storeOptions.store.addAll([ typeT ]);
-    addSimpleFormValue(new rdflib.NamedNode(remoteObjUri), this.storeOptions);
+  // TODO change this to save the correct data in the rdflib form
+  insertRemoteDataObject(remoteObj) {
+    const triples = [
+      {
+        subject: new rdflib.NamedNode(remoteObj.uri),
+        predicate: RDF('type'),
+        object: new rdflib.NamedNode('http://www.semanticdesktop.org/ontologies/2007/03/22/nfo#RemoteDataObject'),
+        graph: this.storeOptions.sourceGraph
+      },
+      {
+        subject: this.storeOptions.sourceNode,
+        predicate: DCT('hasPart'),
+        object: new rdflib.NamedNode(remoteObj.uri),
+        graph: this.storeOptions.sourceGraph
+      },
+      {
+        subject: new rdflib.NamedNode(remoteObj.uri),
+        predicate: NIE('url'),
+        object: remoteObj.address,
+        graph: this.storeOptions.sourceGraph
+      }];
+    this.storeOptions.store.addAll(triples);
   }
 
-  removeRemoteDataObject(remoteObjUri){
-    const typeT = { subject: new rdflib.NamedNode(remoteObjUri),
-                    predicate: RDF('type'),
-                    object: new rdflib.NamedNode('http://www.semanticdesktop.org/ontologies/2007/03/22/nfo#RemoteDataObject'),
-                    graph: this.storeOptions.sourceGraph
-                  };
-    this.storeOptions.store.removeStatements([ typeT ]);
-    removeSimpleFormValue(new rdflib.NamedNode(remoteObjUri), this.storeOptions);
+  // TODO change this to remove the correct data in the rdflib form
+  removeRemoteDataObject(remoteObjUri) {
+    const uri = new rdflib.NamedNode(remoteObjUri);
+    const statements = [
+      ...this.storeOptions.store.match(uri, undefined, undefined, this.storeOptions.sourceGraph),
+      { subject: this.storeOptions.sourceNode, predicate: DCT('hasPart'), object: uri, graph: this.storeOptions.sourceGraph }
+    ];
+    this.storeOptions.store.removeStatements(statements);
   }
 
   @action
@@ -108,33 +134,26 @@ export default class FormInputFieldsRemoteUrlsEditComponent extends Component {
   @action
   async updateRemoteUrl(current, newValue) {
 
-    if (current.remoteUrl && current.remoteUrl.address == newValue.trim()) return; //do nothing if no change
+    if (current.remoteUrl && current.remoteUrl.address === newValue.trim()) return; //do nothing if no change
 
     //for every url update we should make a new remote-url. This is how the model is
     const address = newValue.trim();
     const newRemoteUrl = this.createNewRemoteUrl(address);
 
-    try {
-        await newRemoteUrl.save();
-      } catch (error) {
-        this.remoteErrors.pushObject({resultMessage: "Er ging iets fout bij het opslaan."});
-        return;
-    }
-
     //If there was a previous, remove this from the store
-    if(current.uri){
+    if (current.uri) {
       await current.remoteUrl.destroyRecord();
       this.removeRemoteDataObject(current.uri.value);
     }
 
-    this.insertRemoteDataObject(newRemoteUrl.uri);
+    this.insertRemoteDataObject(newRemoteUrl);
   }
 
   @action
   async removeRemoteUrl(current) {
-    if(current.remoteUrl) {
+    if (current.remoteUrl) {
       await current.remoteUrl.destroyRecord();
-       this.removeRemoteDataObject(current.uri.value);
+      this.removeRemoteDataObject(current.uri.value);
     } else {
       this.remoteUrls = [];
     }
@@ -142,6 +161,7 @@ export default class FormInputFieldsRemoteUrlsEditComponent extends Component {
 
   createNewRemoteUrl(address) {
     return this.store.createRecord('remote-url', {
+      uri: `${REMOTE_URI_TEMPLATE}${uuidv4()}`,
       creator: CREATOR_URI,
       address
     });
