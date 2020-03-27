61c912be03ce0594ab293863e992db0815afdc9e
Erika Pauwels <erika.pauwels@gmail.com>
2020-03-27 16:49:34 +0100

    Remove unused input-field components

--- app/helpers/component-for-display-type-edit.js
+++ app/helpers/component-for-display-type-edit.js
@@ -5,7 +5,6 @@ export default helper(function componentForDisplayTypeEdit(displayTypeUri) {
     'http://lblod.data.gift/display-types/defaultInput' : `form/input-fields/input/edit`,
     'http://lblod.data.gift/display-types/dateTime' : `form/input-fields/date-time/edit`,
     'http://lblod.data.gift/display-types/date' : `form/input-fields/date/edit`,
-    'http://lblod.data.gift/display-types/typeDossierSelect': `form/input-fields/type-dossier-select/edit`,
     'http://lblod.data.gift/display-types/textArea': `form/input-fields/text-area/edit`,
     'http://lblod.data.gift/display-types/conceptSchemeSelector': `form/input-fields/concept-scheme-selector/edit`,
     'http://lblod.data.gift/display-types/files': `form/input-fields/files/edit`,
