328355ef22351eb5317e52e6f681f616efd825d2
benjay10 <ben.behaeghe@redpencil.io>
2022-08-20 03:33:56 +0200

    Repaired constand logging out due to error
    Due to an error in the CurrentSessionService, the application kept
    logging out.

--- app/services/current-session.js
+++ app/services/current-session.js
@@ -1,65 +1,33 @@
 import Service from '@ember/service';
 import { inject as service } from '@ember/service';
-import { task, waitForProperty } from 'ember-concurrency';
+import { tracked } from '@glimmer/tracking';
 
 export default class CurrentSessionService extends Service {
   @service session;
   @service store;
+  @tracked account;
+  @tracked user;
+  @tracked roles;
+  @tracked group;
 
   async load() {
-    if (this.get('session.isAuthenticated')) {
-      const session = this.session;
-      const account = await this.store.find('account', get(session, 'data.authenticated.relationships.account.data.id'));
-      const user = await account.get('gebruiker');
-      const group = await this.store.find('bestuurseenheid', get(session, 'data.authenticated.relationships.group.data.id'));
-      const roles = await get(session, 'data.authenticated.data.attributes.roles');
-      this.set('_account', account);
-      this.set('_user', user);
-      this.set('_roles', roles);
-      this.set('_group', group);
+    if (this.session.isAuthenticated) {
+      this.account = await this.store.find('account', this.session.data.authenticated.relationships.account.data.id);
+      this.user = await this.account.get('gebruiker');
+      this.group = await this.store.find('bestuurseenheid', this.session.data.authenticated.relationships.group.data.id);
+      this.roles = await this.session.data.authenticated.data.attributes.roles;
 
-      // The naming is off, but account,user,roles are taken for the
-      // promises in a currently public API.
-      this.setProperties({
-        accountContent: account,
-        userContent: user,
-        rolesContent: roles,
-        groupContent: group
-      });
-
-      this.set('canAccessToezicht', this.canAccess('LoketLB-toezichtGebruiker'));
-      this.set('canAccessBbcdr', this.canAccess('LoketLB-bbcdrGebruiker'));
-      this.set('canAccessMandaat', this.canAccess('LoketLB-mandaatGebruiker'));
-      this.set('canAccessBerichten', this.canAccess('LoketLB-berichtenGebruiker'));
-      this.set('canAccessLeidinggevenden', this.canAccess('LoketLB-leidinggevendenGebruiker'));
-      this.set('canAccessPersoneelsbeheer', this.canAccess('LoketLB-personeelsbeheer'));
-      this.set('canAccessSubsidies', this.canAccess('LoketLB-subsidies'));
+      this.canAccessToezicht = this.canAccess('LoketLB-toezichtGebruiker');
+      this.canAccessBbcdr = this.canAccess('LoketLB-bbcdrGebruiker');
+      this.canAccessMandaat = this.canAccess('LoketLB-mandaatGebruiker');
+      this.canAccessBerichten = this.canAccess('LoketLB-berichtenGebruiker');
+      this.canAccessLeidinggevenden = this.canAccess('LoketLB-leidinggevendenGebruiker');
+      this.canAccessPersoneelsbeheer = this.canAccess('LoketLB-personeelsbeheer');
+      this.canAccessSubsidies = this.canAccess('LoketLB-subsidies');
     }
   }
 
   canAccess(role) {
-    return this._roles.includes(role);
-  }
-
-  // constructs a task which resolves in the promise
-  @task
-  *makePropertyPromise(property) {
-    yield waitForProperty(this, property);
-    return this.get(property);
-  }
-
-  // this is a promise
-  get account() {
-    return this.makePropertyPromise.perform('_account');
-  }
-
-  // this contains a promise
-  get user() {
-    return this.makePropertyPromise.perform('_user');
-  }
-
-  // this contains a promise
-  get group() {
-    return this.makePropertyPromise.perform('_group');
+    return this.roles.includes(role);
   }
 }
