1b41512ea9908c901afdd6ca3920cbc72ae547ff
benjay10 <ben.behaeghe@redpencil.io>
2022-08-23 00:25:45 +0200

    Correct includes for less requests
    With the necessary includes, the model request for the ember-data-table
    is larger but faster. The data also renders at once instead of in little
    chunks.

--- app/routes/index.js
+++ app/routes/index.js
@@ -16,6 +16,16 @@ export default class IndexRoute extends Route.extend(DataTableRouteMixin) {
         '79a52da4-f491-4e2f-9374-89a13cde8ecd', // Concept status
         '9bd8d86d-bb10-4456-a84e-91e9507c374c', // Sent status
       ].join(','),
+      include: [
+        'form-data',
+        'form-data.passed-by',
+        'form-data.passed-by.is-tijdsspecialisatie-van',
+        'form-data.types',
+        'job',
+        'creator',
+        'status',
+        'last-modifier',
+      ].join(','),
     };
   }
 }
