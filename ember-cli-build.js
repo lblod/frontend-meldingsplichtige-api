ae0474abcd09e8896019a5dfefa7a22b77a2d34b
benjay10 <ben.behaeghe@redpencil.io>
2023-08-08 18:33:01 +0200

    Resolve first deprecation about wormhole

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -11,6 +11,9 @@ module.exports = function (defaults) {
     'ember-simple-auth': {
       useSessionSetupMethod: true,
     },
+    '@appuniversum/ember-appuniversum': {
+      disableWormholeElement: true,
+    },
   });
 
   // Use `app.import` to add additional libraries to the generated
