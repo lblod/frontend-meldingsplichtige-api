bf1eabdfe8cf5c5f27dab26895517d45c6ef4149
benjay10 <ben.behaeghe@redpencil.io>
2023-08-17 14:38:10 +0200

    Add CSS and SCSS linting to release

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,5 +1,12 @@
 'use strict';
 
 module.exports = {
-  extends: ['stylelint-config-standard', 'stylelint-prettier/recommended'],
+  extends: [
+    'stylelint-config-standard',
+    'stylelint-prettier/recommended',
+    'stylelint-config-standard-scss',
+  ],
+  rules: {
+    'selector-class-pattern': '^([a-z][a-z0-9]*)((-)+[a-z0-9]+)*$',
+  },
 };
