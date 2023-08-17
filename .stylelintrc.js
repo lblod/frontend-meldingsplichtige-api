e0ecccb91d0ec55b2fe64c914592023231438373
benjay10 <ben.behaeghe@redpencil.io>
2023-08-17 15:56:00 +0200

    Improved regex class selector, added motivation

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -7,6 +7,8 @@ module.exports = {
     'stylelint-config-standard-scss',
   ],
   rules: {
-    'selector-class-pattern': '^([a-z][a-z0-9]*)((-)+[a-z0-9]+)*$',
+    // `[-_]+` was originally just `-`, but we use `--` and `__` in SCSS to
+    // indicate modifiers and children respectively.
+    'selector-class-pattern': '^([a-z][a-z0-9]*)([-_]+[a-z0-9]+)*$',
   },
 };
