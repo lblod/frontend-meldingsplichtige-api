a5811c01fe845101813b651ce3e787c62d76643e
felix <ruiz.felixruiz@gmail.com>
2026-09-10 13:46:27 +0200

    ensure we know who we are logged in with

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,6 +1,8 @@
 import Controller from '@ember/controller';
+import { inject as service } from '@ember/service';
 
 export default class IndexController extends Controller {
+  @service currentSession;
   page = 0;
   size = 10;
   sort = 'status.label,-sent-date,-modified';
